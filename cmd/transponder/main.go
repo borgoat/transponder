75dc3a2bdfadfd85e60c6845a3e3fffeaa5fcb1b
Giorgio Azzinnaro <giorgio.azzinnaro@gmail.com>
2019-05-09 08:55:21 +0200

    adding some logs to cmd/transponder

--- cmd/transponder/main.go
+++ cmd/transponder/main.go
@@ -23,9 +23,17 @@ var (
 )
 
 func init() {
-	flag.StringVar(&flagData, "data", os.TempDir(), "path where the db file will be stored")
+	flag.StringVar(
+		&flagData,
+		"data",
+		path.Join(os.TempDir(), "transponder"),
+		"path where the db file will be stored")
 
-	flag.StringVar(&flagAddress, "address", ":1492", "address:port to bind the listener")
+	flag.StringVar(
+		&flagAddress,
+		"address",
+		":1492",
+		"address:port to bind the listener")
 }
 
 func main() {
@@ -34,25 +42,36 @@ func main() {
 		flag.Parse()
 	}
 
+	options := file.DefaultOptions
+	options.Directory = flagData
+
+	log.Printf("[DEBUG] Creating gokv.Store with %T %+v", options, options)
+	store, err := file.NewStore(options)
+
+	if err != nil {
+		log.Fatalf("[ERROR] Could not create gokv.Store: %v", err)
+	}
+
+	log.Printf("[DEBUG] Create gokv.Store with type %T", store)
+
+	mgrmap := statemgrmap.NewGoKVMap(store)
+
+	bs := server.NewHTTPBackendServer(mgrmap)
+
+	
 	srv := &http.Server{
 		Addr:         flagAddress,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	options := file.DefaultOptions
-	options.Directory = path.Join(flagData, "transponder")
-	store, _ := file.NewStore(options)
-
-	mgrmap := statemgrmap.NewGoKVMap(store)
-
-	bs := server.NewHTTPBackendServer(mgrmap)
-
 	r := mux.NewRouter()
 	srv.Handler = r
+	log.Printf("[DEBUG] Creating http.Server with options %+v", srv)
 
+	log.Printf("[DEBUG] Handle /terraform with %T", bs)
 	bs.HandleWithRouter(r.PathPrefix("/terraform").Subrouter())
 
-	log.Printf("Starting server on %s", srv.Addr)
+	log.Printf("[INFO] Starting server on %s", srv.Addr)
 	srv.ListenAndServe()
 }
